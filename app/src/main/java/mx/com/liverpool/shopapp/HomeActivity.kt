324ba36da038453345c65ea31d7f18fc5aa814d1
moonsmileh <moonsmileh@gmail.com>
2024-07-19 10:43:51 -0600

    Initial commit

---
+++ app/src/main/java/mx/com/liverpool/shopapp/HomeActivity.kt
@@ -0,0 +1,19 @@
+package mx.com.liverpool.shopapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import mx.com.liverpool.shopapp.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
